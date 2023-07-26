887b4051619da6fb332605edb6765eeb094fb0c0
Retanar <59249123+retanar@users.noreply.github.com>
2023-07-26 14:05:18 +0300

    Added navigation

--- app/src/main/java/retanar/test/chi/MainActivity.kt
+++ app/src/main/java/retanar/test/chi/MainActivity.kt
@@ -1,18 +1,12 @@
 package retanar.test.chi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val button = findViewById<Button>(R.id.openFragmentButton)
-        button.setOnClickListener {}
     }
 }
--- app/src/main/java/retanar/test/chi/MainFragment.kt
+++ app/src/main/java/retanar/test/chi/MainFragment.kt
@@ -6,30 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
-class CountingFragment : Fragment() {
-
-    private var counter = 0
+class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_counting, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val textView = view.findViewById<TextView>(R.id.fragmentCounterTextView)
-        val button = view.findViewById<Button>(R.id.incrementButton)
+        val textView = view.findViewById<TextView>(R.id.mainCounterTextView)
+        val button = view.findViewById<Button>(R.id.openFragmentButton)
 
-        textView.text = counter.toString()
         button.setOnClickListener {
-            incrementCounter(textView)
+            findNavController().navigate(R.id.action_mainFragment_to_countingFragment)
         }
-    }
-
-    private fun incrementCounter(textView: TextView) {
-        counter += 1
-        textView.text = counter.toString()
     }
 }
